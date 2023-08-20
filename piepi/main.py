cc6eaa17822edf0f3414e0352a80f9cd7fbd76b9
Bingsu <ks2515@naver.com>
2023-08-20 21:41:22 +0900

    feat: orjson

--- piepi/main.py
+++ piepi/main.py
@@ -1,6 +1,26 @@
+import orjson
 from ninja import NinjaAPI
+from ninja.parser import Parser
+from ninja.renderers import BaseRenderer
+from ninja.types import DictStrAny
+from pydantic import BaseModel
 
-from server.upload import router as upload_router
+from server.main import router
 
-api = NinjaAPI()
-api.add_router("", upload_router)
+
+class ORJSONRenderer(BaseRenderer):
+    media_type = "application/json"
+
+    def render(self, request, data, *, response_status):
+        if isinstance(data, BaseModel):
+            data = data.model_dump()
+        return orjson.dumps(data)
+
+
+class ORJSONParser(Parser):
+    def parse_body(self, request) -> DictStrAny:
+        return orjson.loads(request.body)
+
+
+api = NinjaAPI(renderer=ORJSONRenderer(), parser=ORJSONParser())
+api.add_router("", router)
