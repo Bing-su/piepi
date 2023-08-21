e50e10c9f1a3a4b14b2d27fa9197362416d0be8a
Bingsu <ks2515@naver.com>
2023-08-21 20:41:20 +0900

    fix: logging, secret key

--- piepi/settings_docker.py
+++ piepi/settings_docker.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
+from django.utils.log import DEFAULT_LOGGING
 from dotenv import load_dotenv
 
 from .settings import *  # noqa: F403
@@ -14,11 +15,11 @@ LANGUAGE_CODE = os.getenv("LANGUAGE_CODE", "en-us")
 TIME_ZONE = os.getenv("TIME_ZONE", "UTC")
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
-    secret_key = get_random_secret_key()
+    SECRET_KEY = get_random_secret_key()
     with BASE_DIR.joinpath(".env").open("a", encoding="utf-8") as file:
-        file.write(f"\nSECRET_KEY={secret_key}\n")
-    os.environ["SECRET_KEY"] = secret_key
-    del secret_key
+        file.write(f"\nSECRET_KEY={SECRET_KEY}\n")
+    os.environ["SECRET_KEY"] = SECRET_KEY
 
 default_media_root = Path.home().joinpath("packages").as_posix()
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", default_media_root)
+DEFAULT_LOGGING["handlers"]["console"]["filters"].clear()
