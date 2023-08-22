5157af7b0a393aedfdcad495730a387197c0040b
Dowon <ks2515@naver.com>
2023-08-22 10:51:28 +0900

    fix: set .env

--- piepi/settings_docker.py
+++ piepi/settings_docker.py
@@ -3,11 +3,14 @@ from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 from django.utils.log import DEFAULT_LOGGING
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv, set_key
 
 from .settings import *  # noqa: F403
 
-load_dotenv()
+dotenv_path = Path(find_dotenv() or Path(__file__).parent.parent.joinpath(".env"))
+dotenv_path.touch(exist_ok=True)
+load_dotenv(dotenv_path)
+
 DEBUG = False
 BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split()
@@ -16,9 +19,7 @@ TIME_ZONE = os.getenv("TIME_ZONE", "UTC")
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     SECRET_KEY = get_random_secret_key()
-    with BASE_DIR.joinpath(".env").open("a", encoding="utf-8") as file:
-        file.write(f"\nSECRET_KEY={SECRET_KEY}\n")
-    os.environ["SECRET_KEY"] = SECRET_KEY
+    set_key(dotenv_path, "SECRET_KEY", SECRET_KEY)
 
 default_media_root = BASE_DIR.joinpath("packages").as_posix()
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", default_media_root)
