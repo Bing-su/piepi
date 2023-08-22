0061b4ccff766e00d6353fdf28ce5fec1552778b
Bingsu <ks2515@naver.com>
2023-08-22 22:17:01 +0900

    fix: create super user move to config

--- piepi/wsgi_docker.py
+++ piepi/wsgi_docker.py
@@ -9,15 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "piepi.settings_docker")
 
 application = get_wsgi_application()
-
-if not User.objects.all().exists():
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin")
-    User.objects.create_superuser(username, email, password)
